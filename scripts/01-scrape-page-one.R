96f86c5af526a16338667bc9bcce9408d3f3f60b
Elayna Seago <elaynaseago@gmail.com>
2022-03-08 14:33:31 -0500

    page one and two art

--- scripts/01-scrape-page-one.R
+++ scripts/01-scrape-page-one.R
@@ -27,34 +27,57 @@ links <- page %>%
   html_attr("href") %>%
   str_replace("\\.", "___")
 
-titles <- page %>%
-  html_nodes(".iteminfo") %>%
-  html_node("h3 a") %>%
-  html_text() %>%
-  str_squish()
 
 # scrape artists ---------------------------------------------------------------
 
 artists <- page %>%
   html_nodes(".iteminfo") %>%
   html_node(".artist") %>%
-  ___
+  html_text() %>%
+  str_squish()
 
 # put together in a data frame -------------------------------------------------
 
 first_ten <- tibble(
-  title = ___,
-  artist = ___,
-  link = ___
+  title = titles,
+  artist = artists,
+  link = links
 )
 
 # scrape second ten paintings --------------------------------------------------
 
-second_url <- "___"
+second_url <- "https://collections.ed.ac.uk/art/search/*:*/Collection:%22edinburgh+college+of+art%7C%7C%7CEdinburgh+College+of+Art%22?offset=10"
 
 page <- read_html(second_url)
-...
+
+# titles -----------------------------------------------------------------
+titles <- page %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_text() %>%
+  str_squish()
+
+#links -----------------------------------------------------------------
+
+links <- page %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_attr("href") %>%
+  str_replace("\\.", "___")
+
+#artists-----------------------------------------------------------------
+
+artists <- page %>%
+  html_nodes(".iteminfo") %>%
+  html_node(".artist") %>%
+  html_text() %>%
+  str_squish()
+
+#tibble ---------------------------------------------------------------------
 
 second_ten <- tibble(
-  ...
+  title = titles,
+  artist = artists,
+  link = links
 )
+
